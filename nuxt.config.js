610a370b7989aa8c1d53ffdbd06d5b33f4a4fb3b
caosiwei <1286612323@qq.com>
2021-09-18 00:02:42 +0800

    添加server配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,5 +71,10 @@ export default {
     ],
   },
 
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost,
+  },
+
   plugins: ['~/plugins/axios.js', '~/plugins/dayjs.js'],
 }
